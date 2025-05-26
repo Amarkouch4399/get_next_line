45d1a19037c0a7b0dca87e5e722c724be8c3e19f
Amarkouch4399 <ouamarko@student.s19.be>
2025-05-26 18:59:08 +0200

    1

--- get_next_line.c
+++ get_next_line.c
@@ -6,43 +6,41 @@
 /*   By: ouamarko <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/20 10:06:42 by ouamarko          #+#    #+#             */
-/*   Updated: 2025/05/25 19:18:42 by ouamarko         ###   ########.fr       */
+/*   Updated: 2025/05/26 18:45:14 by ouamarko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-/*
-char	*ft_get_next_line(int fd)
-{
-	static int	buffer_size;
-	static char	*buffer[buffer_size];
 
-	if (fd < 0)
-		return (-1);
-	
-
-
-}
-int	ft_buff_size(int fd)
-{
-	int	i;
-
-	i = 0;
-	if (fd < 0)
-		return (-1);
-	while (fd[i] != '\n' ||)
-		i++;
-	return (i);
-}
-*/
 #include <fcntl.h>
 #include <stdio.h>
 #include <unistd.h>
+
+int	ft_strlen(int fd)
+{
+	int	i;
+	ssize_t	sz;
+	char	*s;
+	i = 0;
+	while (sz = read(fd, &s[i] ,i) != EOF)
+		i++;
+	return (i);
+}
+char	*ft_get_next_line(int fd)
+{
+	int	buffer_size;
+	static const char	*buffer[1000];
+
+	buffer_size = 0;
+	buffer_size = ft_strlen(fd);
+	return (0);
+}
 int	main()
 {
 	int	fd;
-	char	*c = "OUSSAMA\namarkouch\n123456\n123";
+	static char	*c = "OUSSAMA\namarkouch\n123456\n123";
 
 	fd = open("test.txt", O_CREAT | O_WRONLY);
 	if (fd < 0)
 		return(-1);
-	printf("%d\n", fd);
+	ft_get_next_line(fd);
+	printf("%d\n", ft_strlen(fd));
 }
